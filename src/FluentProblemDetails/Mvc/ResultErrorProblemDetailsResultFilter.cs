179377e5d5106c9b4884f6ce65fdb0cccd00b108
Greg White <greg@techcentric.co.nz>
2024-02-12 16:25:46 +1300

    Added support for non-reference type Result values and Results with no value

--- src/FluentProblemDetails/Mvc/ResultErrorProblemDetailsResultFilter.cs
+++ src/FluentProblemDetails/Mvc/ResultErrorProblemDetailsResultFilter.cs
@@ -1,4 +1,7 @@
-﻿using FluentResults;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using FluentResults;
 using Hellang.Middleware.ProblemDetails;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -23,6 +26,8 @@ namespace FluentProblemDetails.Mvc
             Options = options.Value;
         }
 
+        #region Implementation of IActionFilter
+        
         public void OnActionExecuted(ActionExecutedContext context)
         {
             if (context.Result is not ObjectResult objectResult)
@@ -30,40 +35,90 @@ namespace FluentProblemDetails.Mvc
                 return;
             }
 
-            if (objectResult.Value is IResult<object> result)
+            if (objectResult.Value is IResultBase result)
             {
-                if (!result.IsSuccess)
-                {
-                    var problemDetails = Factory.Create(result.Errors);
-
-                    object returningResult;
-                    if (problemDetails.Exception is not null)
-                    {
-                        returningResult = new ProblemDetailsException(problemDetails, problemDetails.Exception);
-                    }
-                    else
-                    {
-                        returningResult = problemDetails;
-                    }
-
-                    context.Result = new ObjectResult(returningResult)
-                    {
-                        StatusCode = problemDetails.Status
-                    };
-                }
-                else
-                {
-                    context.Result = new ObjectResult(result.Value);
-                }
+                context.Result = HandleResult(result);
             }
-
-
         }
+
 
         public void OnActionExecuting(ActionExecutingContext context)
         {
-
         }
+
+        #endregion  
+
+
+
+        /// <summary>
+        /// Handles the result.
+        /// </summary>
+        /// <param name="result">The result.</param>
+        /// <returns></returns>
+        private IActionResult HandleResult(IResultBase result)
+        {
+            if (!result.IsSuccess)
+            {
+                return HandleFailedResult(result);
+            }
+
+            // Extract out the result value and return it - this avoids any superfluous IResultBase members from being serialised to the response output.
+            var successValue = ExtractValueFromResult(result);
+
+            return new ObjectResult(successValue ?? null);
+        }
+        
+
+        private IActionResult HandleFailedResult(IResultBase result)
+        {
+            var problemDetails = Factory.Create(result.Errors);
+
+            object returningResult;
+            if (problemDetails.Exception is not null)
+            {
+                returningResult = new ProblemDetailsException(problemDetails, problemDetails.Exception);
+            }
+            else
+            {
+                returningResult = problemDetails;
+            }
+
+            return new ObjectResult(returningResult)
+            {
+                StatusCode = problemDetails.Status
+            };
+        }
+
+
+        private object ExtractValueFromResult(IResultBase result)
+        {
+            // If the result value is a reference type then covariance will let us access it via IResult<object>
+            if (result is IResult<object> valueResult)
+            {
+                return valueResult.Value;
+            }
+
+            // But what if the result value is a value type (eg: int or enum)
+            var resultType = result.GetType();
+            var genericResultInterface = resultType
+                .GetInterfaces()
+                .FirstOrDefault(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IResult<>));
+
+            if (genericResultInterface != null)
+            {
+                var valueProperty = genericResultInterface.GetProperty(nameof(IResult<object>.Value), BindingFlags.Instance | BindingFlags.Public);
+
+                if (valueProperty == null)
+                    throw new Exception($"Failed to find Value property on type {resultType}.");
+
+                var value = valueProperty.GetValue(result);
+
+                return value;
+            }
+            
+            return null;
+        }
+
 
 
 
